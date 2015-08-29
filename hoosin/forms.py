597930b4aa56c9d6a4bb92124a694d97536a07e2
Brandon Purvis <bcp7cc@virginia.edu>
2015-08-29 13:57:14 -0400

    fix database

--- hoosin/forms.py
+++ hoosin/forms.py
@@ -1,11 +1,5 @@
 from django import forms
-from hoosin import models
+
 
 class SearchForm(forms.Form):
     query = forms.CharField()
-
-
-class SubmitInfoForm(forms.ModelForm):
-    class Meta:
-        model = models.HoursEnrty
-        exclude = []
