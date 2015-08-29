1fe923ddd2a869870041c596e3e258bec4e55c7c
Brandon Purvis <bcp7cc@virginia.edu>
2015-08-29 14:20:00 -0400

    merged

--- hoosin/views.py
+++ hoosin/views.py
@@ -4,8 +4,10 @@ from hoosin import models
 
 
 def default(request):
+    test_enrty = models.HoursEntry(professor='Joe Testum', course='TST 2102', hours='MWF:10-1,2-3,4-5')
+    test_enrty.save()
+
     context = {}
     context.update({'search_form': forms.SearchForm()})
     context.update({'update_form': models.HoursEntryModelForm()})
-    context.update({'entry_count': len(models.HoursEntry.objects.all())})
     return render(request, 'hoos/main.html', context=context)
