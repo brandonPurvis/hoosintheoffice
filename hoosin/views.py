3de9dc7a17dd1a8323a647e3f7f4eb9c91e894ff
Brandon Purvis <bcp7cc@virginia.edu>
2015-08-29 14:33:58 -0400

    re-add entry count

--- hoosin/views.py
+++ hoosin/views.py
@@ -10,4 +10,5 @@ def default(request):
     context = {}
     context.update({'search_form': forms.SearchForm()})
     context.update({'update_form': models.HoursEntryModelForm()})
+    context.update({'entry_count': len(models.HoursEntry.objects.all())})
     return render(request, 'hoos/main.html', context=context)
