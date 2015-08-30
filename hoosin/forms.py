54e2e57a3f288fe102ad74e742c21d976de52098
Brandon Purvis <bcp7cc@virginia.edu>
2015-08-30 14:03:14 -0400

    Improve appearance of new hours form page.

--- hoosin/forms.py
+++ hoosin/forms.py
@@ -1,35 +1,39 @@
 from django import forms
 
 
-class SearchForm(forms.Form):
-    query = forms.CharField(widget=forms.TextInput(attrs={
+def build_text_form_control_widget(place_holder=None):
+    return forms.TextInput(attrs={
         'class': 'form-control',
-        'placeholder': 'Search',
-    }))
+        'placeholder':  place_holder or ''
+    })
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(widget=build_text_form_control_widget('Search'))
 
 
 class NewHoursForm(forms.Form):
-    professor = forms.CharField(max_length=50)
-    course = forms.CharField(max_length=50)
-    office = forms.CharField(max_length=50)
+    professor = forms.CharField(max_length=50, widget=build_text_form_control_widget('Professor'))
+    course = forms.CharField(max_length=50, widget=build_text_form_control_widget('Course'))
+    office = forms.CharField(max_length=50, widget=build_text_form_control_widget('Office'))
 
-    monday_start = forms.TimeField(required=False)
-    monday_stop = forms.TimeField(required=False)
+    monday_start = forms.TimeField(required=False, widget=build_text_form_control_widget('start'))
+    monday_stop = forms.TimeField(required=False, widget=build_text_form_control_widget('end'))
 
-    tuesday_start = forms.TimeField(required=False)
-    tuesday_stop = forms.TimeField(required=False)
+    tuesday_start = forms.TimeField(required=False, widget=build_text_form_control_widget('start'))
+    tuesday_stop = forms.TimeField(required=False, widget=build_text_form_control_widget('end'))
 
-    wednesday_start = forms.TimeField(required=False)
-    wednesday_stop = forms.TimeField(required=False)
+    wednesday_start = forms.TimeField(required=False, widget=build_text_form_control_widget('start'))
+    wednesday_stop = forms.TimeField(required=False, widget=build_text_form_control_widget('end'))
 
-    thursday_start = forms.TimeField(required=False)
-    thursday_stop = forms.TimeField(required=False)
+    thursday_start = forms.TimeField(required=False, widget=build_text_form_control_widget('start'))
+    thursday_stop = forms.TimeField(required=False, widget=build_text_form_control_widget('end'))
 
-    friday_start = forms.TimeField(required=False)
-    friday_stop = forms.TimeField(required=False)
+    friday_start = forms.TimeField(required=False, widget=build_text_form_control_widget('start'))
+    friday_stop = forms.TimeField(required=False, widget=build_text_form_control_widget('end'))
 
-    saturday_start = forms.TimeField(required=False)
-    saturday_stop = forms.TimeField(required=False)
+    saturday_start = forms.TimeField(required=False, widget=build_text_form_control_widget('start'))
+    saturday_stop = forms.TimeField(required=False, widget=build_text_form_control_widget('end'))
 
-    sunday_start = forms.TimeField(required=False)
-    sunday_stop = forms.TimeField(required=False)
+    sunday_start = forms.TimeField(required=False, widget=build_text_form_control_widget('start'))
+    sunday_stop = forms.TimeField(required=False, widget=build_text_form_control_widget('end'))
