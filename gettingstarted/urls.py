7451da49b8317953ac93220d342e39b99271e3da
Brandon Purvis <bcp7cc@virginia.edu>
2015-09-08 16:52:39 -0400

    Add admin site.

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 import hoosin.views
 
 urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', hoosin.views.default, name='index'),
     url(r'^new_hours_form', hoosin.views.go_to_office_hours_form),
     url(r'^new_hours_submit', hoosin.views.new_hours_submission_hook, name='new_hours'),
